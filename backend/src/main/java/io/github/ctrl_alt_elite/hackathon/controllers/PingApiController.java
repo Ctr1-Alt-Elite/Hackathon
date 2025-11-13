fb6ea520a00f78c69288e4035f1b0c5ea4aea27a
AlexeyZarechnev <408642@niuitmo.ru>
2025-11-13 18:59:37 +0300

    init backend

---
+++ backend/src/main/java/io/github/ctrl_alt_elite/hackathon/controllers/PingApiController.java
@@ -0,0 +1,52 @@
+package io.github.ctrl_alt_elite.hackathon.controllers;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.context.request.NativeWebRequest;
+
+import jakarta.validation.constraints.*;
+import jakarta.validation.Valid;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import jakarta.annotation.Generated;
+
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2025-11-13T18:38:29.401150+03:00[Europe/Moscow]", comments = "Generator version: 7.8.0")
+@Controller
+@RequestMapping("${openapi.hackathon.base-path:}")
+public class PingApiController implements PingApi {
+
+    private final NativeWebRequest request;
+
+    @Autowired
+    public PingApiController(NativeWebRequest request) {
+        this.request = request;
+    }
+
+    @Override
+    public Optional<NativeWebRequest> getRequest() {
+        return Optional.ofNullable(request);
+    }
+
+    @GetMapping("/ping")
+    public ResponseEntity<String> pong() {
+        return ResponseEntity.ok().body("pong");
+    }
+
+}
