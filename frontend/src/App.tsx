fe1b9f883a6e157749b723b74901a0973bfdf32a
AlexeyZarechnev <144028600+AlexeyZarechnev@users.noreply.github.com>
2025-11-15 15:34:01 +0300

    add search and style for it (#9)
    Authored-by: YaroslavaGalimova <yara.olympiads@gmail.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,43 @@
 // import TestConnection from './TestButtons';
 // import './App.css';
+import "./styles/Navigation.css"
+import "./styles/Search.css"
 
-import TestConnection from "./TestButtons";
+import Account from "./components/Account";
+import Search from "./components/Search";
+import TestConnection from "./components/TestButtons";
 
 function App() {
   return (
+    <>
+    <head>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+      <link href="https://fonts.googleapis.com/css2?family=Momo+Trust+Display&display=swap" rel="stylesheet" />
+    </head>
     <div className="App">
-      <h1>BuildTech Base</h1>
-      <header>
-        <ul>
-          <li>Ссылка на сервис1</li>
-          <li>Ссылка на сервис2</li>
-          <li>Ссылка на сервис3</li>
-        </ul>
-        <div>
-          Тут будет вход в аккаунт
-        </div>
-      </header>
-      <body>
+      <div className="topApp">
+        <h1>BuildTech Base</h1>
+        <header>
+          <ul className="navigation">
+            <li><a href="#" className="active">Главная</a></li>
+            <li><a href="#">Мои документы</a></li>
+            <li><a href="#">Комментарии</a></li>
+          </ul>
+          <div>
+            <Account />
+          </div>
+        </header>
+      </div>
+      <div className="main-page-body">
         <TestConnection />
-        Строка поиска
-        Список рекомендаций
-      </body>
+        <Search />
+      </div>
       <aside>
         
       </aside>
     </div>
+    </>
   );
 }
 
